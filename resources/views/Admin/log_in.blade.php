1f163e5aa2e7b48c5b5ff1ef20400e6ab17942e7
lumeida <lumeida@localhost.localdomain>
2020-02-26 18:37:44 +0100

    Mise en place page d'erreur

--- resources/views/Admin/log_in.blade.php
+++ resources/views/Admin/log_in.blade.php
@@ -10,23 +10,23 @@
                         <div class="card-body">
 
                             <!-- Header -->
-                            <div class="form-header indigo">
+                            <div class="form-header indigo darken-4">
                                 <h3 class="font-weight-500 my-2 py-1"><i class="fas fa-user"></i> Connexion </h3>
                             </div>
 
                             <!-- Body -->
                             <div class="md-form">
-                                <i class="fas fa-envelope prefix indigo-text"></i>
+                                <i class="fas fa-envelope prefix indigo-text darken-4"></i>
                                 <input type="text" id="orangeForm-name" class="form-control" placeholder="Votre email">
                             </div>
 
                             <div class="md-form">
-                                <i class="fas fa-lock prefix indigo-text"></i>
+                                <i class="fas fa-lock prefix indigo-text darken-4"></i>
                                 <input type="password" id="orangeForm-pass" class="form-control" placeholder="Votre mot de passe">
                             </div>
 
                             <div class="text-center">
-                                <button  class=" btn indigo btn-lg " type="submit">Valider</button>
+                                <button  class=" btn indigo btn-lg darken-4" type="submit">Valider</button>
                                 <hr class="mt-4">
                                 <div class="inline-ul text-center d-flex justify-content-center">
                                     <a class="p-2 m-2 fa-lg darken-4 indigo-text" style="font-size: 15px;" id="submit"><span>Mot de passe oublié ?</span></a>
